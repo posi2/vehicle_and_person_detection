4ec8fd1aa7fa925d93bd6e65771d14f86342bb47
dikshap07 <51503505+dikshap07@users.noreply.github.com>
2019-09-30 22:41:14 +0530

    Add files via upload

---
+++ code/yolo_video.py
@@ -0,0 +1,34 @@
+import sys
+import argparse
+from yolo import YOLO, detect_video
+from PIL import Image
+
+FLAGS = None
+
+if __name__ == '__main__':
+    # class YOLO defines the default value, so suppress any default here
+    parser = argparse.ArgumentParser(argument_default=argparse.SUPPRESS)
+    '''
+    Command line options
+    '''
+    parser.add_argument('--model', type=str,help='path to model weight file, default ' + YOLO.get_defaults("model_path") )
+
+    parser.add_argument('--anchors', type=str,help='path to anchor definitions, default ' + YOLO.get_defaults("anchors_path"))
+
+    parser.add_argument('--classes', type=str,help='path to class definitions, default ' + YOLO.get_defaults("classes_path"))
+
+    parser.add_argument('--gpu_num', type=int,help='Number of GPU to use, default ' + str(YOLO.get_defaults("gpu_num")))
+    '''
+    Command line positional arguments -- for video detection mode
+    '''
+    parser.add_argument("--input", nargs='?', type=str,required=False,default='./path2your_video',help = "Video input path")
+
+    parser.add_argument("--output", nargs='?', type=str, default=".\video_out.mp4",help = "[Optional] Video output path")
+
+    FLAGS = parser.parse_args()
+
+    
+    if "input" in FLAGS:
+        detect_video(YOLO(**vars(FLAGS)), FLAGS.input, FLAGS.output)
+    else:
+        print("Must specify at least video_input_path.  See usage with --help.")
